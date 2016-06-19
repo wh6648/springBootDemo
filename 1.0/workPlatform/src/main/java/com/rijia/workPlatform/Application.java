e73b359978e234cb504a9ee94fa1c1bf065e446d
wh6648 <wh6648@hotmail.com>
2016-06-19 18:52:45 +0800

    打war包

--- 1.0/workPlatform/src/main/java/com/rijia/workPlatform/Application.java
+++ 1.0/workPlatform/src/main/java/com/rijia/workPlatform/Application.java
@@ -2,14 +2,21 @@ package com.rijia.workPlatform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan("com.rijia.workPlatform")
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
-  public static void main(String[] args) {
-    SpringApplication.run(Application.class, args);
-  }
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
 
 }
